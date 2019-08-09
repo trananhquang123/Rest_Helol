95431dd65d5ca80c09a67c47e96fe23c83e243be
Quang-Tran <Trananhquang197@gmail.com>
2019-08-09 11:50:40 +0700

    add project

---
+++ src/main/java/com/example/entitys/hello_Entity.java
@@ -0,0 +1,31 @@
+package com.example.entitys;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "hello_world")
+public class hello_Entity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "usersgenerator")
+    private Long id;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Column(name = "message")
+    private String message;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
