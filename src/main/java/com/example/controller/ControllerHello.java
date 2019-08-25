46d304cc2174467817d8ff4b79653689e3ef2ff8
Quang-Tran <Trananhquang197@gmail.com>
2019-08-26 00:15:33 +0700

    add

---
+++ src/main/java/com/example/controller/ControllerHello.java
@@ -0,0 +1,42 @@
+package com.example.controller;
+
+import com.example.entites.HelloEntity;
+import com.example.service.ServiceHello;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/helloworld")
+public class ControllerHello {
+
+    @Autowired
+    ServiceHello serviceHello;
+
+    @GetMapping("/")
+    public List<HelloEntity> hello(){
+       return serviceHello.hello();
+    }
+
+    @PostMapping("/add-hello")
+    public ResponseEntity<HelloEntity> addHello(@RequestBody HelloEntity addhello){
+        HelloEntity  hello = serviceHello.save(addhello);
+
+        return ResponseEntity.ok().body(hello);
+    }
+    @PutMapping("/update/{id}")
+    public HelloEntity updateId(@PathVariable(value ="id") long id){
+        Optional<HelloEntity> findId =serviceHello.findById(id);
+        if(findId.isPresent()){
+            return null;
+        }
+        serviceHello.updateHelloById(id,"Hola");
+        Optional<HelloEntity> findIdUpdated =serviceHello.findById(id);
+
+         return findIdUpdated.get();
+    }
+
+}
